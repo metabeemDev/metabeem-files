f808defa62e26a659eea9f006638cd51e92c90b5
stevendeyou <stevendeyou@outlook.com>
2024-03-19 23:03:12 +0800

    Added SkyWalking

--- src/main.js
+++ src/main.js
@@ -1,3 +1,32 @@
+import assert from 'assert';
+import _ from 'lodash';
+import agent from 'skywalking-backend-js';
+
+import 'deyml/config';
 import { runHttp } from "./http.js";
 
-runHttp().then( _res => {} ).catch( err => console.error( err ) );
+
+async function asyncMain()
+{
+	/**
+	 * 	install SkyWalking anent
+	 */
+	assert.strictEqual( true, _.isString( process.env.SW_AGENT_NAME ) && ! _.isEmpty( process.env.SW_AGENT_NAME ) );
+	assert.strictEqual( true, _.isString( process.env.SW_AGENT_INSTANCE_NAME ) && ! _.isEmpty( process.env.SW_AGENT_INSTANCE_NAME ) );
+	assert.strictEqual( true, _.isString( process.env.SW_AGENT_COLLECTOR_BACKEND_SERVICES ) && ! _.isEmpty( process.env.SW_AGENT_COLLECTOR_BACKEND_SERVICES ) );
+	const agentInstance = agent.hasOwnProperty( 'default' ) ? agent.default : agent;
+	agentInstance.start({
+		serviceName: process.env.SW_AGENT_NAME,
+		serviceInstance: process.env.SW_AGENT_INSTANCE_NAME,
+		collectorAddress: process.env.SW_AGENT_COLLECTOR_BACKEND_SERVICES,
+		authorization: ""
+	});
+
+	/**
+	 * 	start relay
+	 */
+	await runHttp();
+}
+
+
+asyncMain().then( res =>{} ).catch( err => { console.error( err ) } );
